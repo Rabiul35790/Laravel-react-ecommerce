1a8d0433768df98ff0c61925dc669e8f3b2a0a1a
Rabiul35790 <roniahmed35790@gmail.com>
2025-04-25 21:33:38 +0600

    com

--- app/Models/Department.php
+++ app/Models/Department.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 class Department extends Model
 {
     public function categories(): HasMany {
+        //
         return $this->hasMany(Category::class);
     }
 }
